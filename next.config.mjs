cdae4763a78d0215d57e41ac9dc44b44e30d8a41
sawnannmon <sawnannmon@futurehubmyanmar.com>
2026-04-27 10:35:09 +0630

    update-loading-speed

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@tabler/icons-react'],
+  },
 };
 
 export default nextConfig;
